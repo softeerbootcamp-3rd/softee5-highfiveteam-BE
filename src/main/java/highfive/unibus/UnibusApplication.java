be23851467a396c8fab5a5fd01a0c517626391c1
kelly1422 <kelly1422@naver.com>
2024-01-04 19:43:05 +0900

    [init]: project setting

---
+++ src/main/java/highfive/unibus/UnibusApplication.java
@@ -0,0 +1,13 @@
+package highfive.unibus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnibusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnibusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/highfive/unibus/UnibusApplicationTests.java
@@ -0,0 +1,13 @@
+package highfive.unibus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnibusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
